193224af0a7c3ef6742349d1b852c2aeb5a2e345
Cocozzj <zhan7565@umn.edu>
2024-02-20 23:27:01 -0800

    update

--- requirements.ps1
+++ requirements.ps1
@@ -43,10 +43,10 @@ cd ./GPT_code_pub
 ### under GPT_code_pub folder
 scp Web_data/GPTs_info/* gpts@cs-pluto.cs.umn.edu:~/upload_data/GPTs_info/
 
-# Thinkpad: C:\Program Files\Google\Chrome\Application>chrome.exe --remote-debugging-port=9223 --user-data-dir="C:\Users\cocoz\Downloads\selenium\selenium1\ChromeProfile"
-# VMare: C:\Program Files\Google\Chrome\Application>chrome.exe --remote-debugging-port=9223 --user-data-dir="C:\Users\Zejun\Downloads\selenium\selenium1\ChromeProfile"
-# Lenovo(ps): C:\Program Files\Google\Chrome\Application>.\chrome.exe --remote-debugging-port=9223 --user-data-dir='C:\Downloads\selenium\selenium1\ChromeProfile'
-# Anlan(ps) C:\Program Files (x86)\Google\Chrome\Application>.\chrome.exe --remote-debugging-port=9223 --user-data-dir='C:\Downloads\selenium\selenium1\ChromeProfile'
+# Thinkpad: 'C:\Program Files\Google\Chrome\Application'>.\chrome.exe --remote-debugging-port=9223 --user-data-dir="C:\Users\cocoz\Downloads\selenium\selenium1\ChromeProfile"
+# VMare: 'C:\Program Files\Google\Chrome\Application'>.\chrome.exe --remote-debugging-port=9223 --user-data-dir="C:\Users\Zejun\Downloads\selenium\selenium1\ChromeProfile"
+# Lenovo(ps): 'C:\Program Files\Google\Chrome\Application'>.\chrome.exe --remote-debugging-port=9223 --user-data-dir='C:\Downloads\selenium\selenium1\ChromeProfile'
+# Anlan(ps) 'C:\Program Files (x86)\Google\Chrome\Application'>.\chrome.exe --remote-debugging-port=9223 --user-data-dir='C:\Downloads\selenium\selenium1\ChromeProfile'
 
 
 chrome://settings/clearBrowserData
